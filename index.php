0dc4981ab5fc5ed132dd2218b057fadd1c2dd7c1
Paul <paul.benier@efrei.net>
2021-11-08 21:32:53 +0000

    Ajout fichiers footer header style et modification index

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Title of the web page</title>
+        <link rel="stylesheet" href="style.css" type="text/css">
+    </head>
+    <body>
+        <!-- ##### HEADER ##### -->
+        <?php include('header.php'); ?>
+        <!-- ##### end - HEADER ##### -->
+        This is the main page
+        <!-- ##### FOOTER ##### -->
+        <?php include('footer.php'); ?>
+        <!-- ##### end - FOOTER ##### -->
+    </body>
+</html>
+
 <?php ?>
