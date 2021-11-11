670130a4ab4469cedfe1001eeaeb142ebff136bb
BENIER <sbenier@icloud.com>
2021-11-11 09:57:20 +0000

    navigation bar

--- header.php
+++ header.php
@@ -1,3 +1,8 @@
 <body>
-    This is the header
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="memebers.php">Members</a></li>
+        <li><a href="messaging.php">Messaging</a></li>
+        <li><a href="profile.php">My profile</a></li>
+    </ul> 
 </body>
