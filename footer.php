15e0afcf0cd9b34adaa55889c285de989e48300a
Paul <paul.benier@efrei.net>
2021-11-24 23:24:39 +0000

    footer

--- footer.php
+++ footer.php
@@ -1,1 +1,3 @@
-This is the footer
+Social Networking website project by Paul, Séverine, Huiting, Maël and Logan.<br>
+Github : <a href="https://github.com/Paul-Benier/Social_Networking" target="_blank">https://github.com/Paul-Benier/Social_Networking</a><br>
+Staffordshire University x Efrei Paris
